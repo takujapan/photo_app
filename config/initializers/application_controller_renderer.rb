afb6d0feebea3e6718fad7ff30488312da22992f
Taku <takujapan1994@gmail.com>
2020-10-22 04:21:32 +0000

    ユーザーページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '37cd06c7bf8a5dd54ccf41c7494ddc6ae62d689dcf7ee79a0b7eb3eb1ed2f486c20f3791bf426599eb8aad865c500c83f43eb6598ae0c57fb554c7af602dac7c'
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get homes_index_url
+    get home_index_url
     assert_response :success
   end
 
