0115e22ef62ac45ab207d58bbdefff2d84ee4f02
Taku <takujapan1994@gmail.com>
2020-10-26 06:04:54 +0000

    トップページ変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
-    users_path(resource)
+    photos_path
   end
   
   def after_update_path_for(resource)
-    users_path(resource)
+    photos_path
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-    users_path(resource)
+    photos_path
   end 
   
   def after_sign_out_path_for(resource)
-   root_path
+    photos_path
   end 
 end
